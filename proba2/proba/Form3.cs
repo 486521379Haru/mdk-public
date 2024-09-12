0719cea31c5f0134114f2fb8ed7391d11b054581
Haru_Marden <133318413+486521379Haru@users.noreply.github.com>
2024-09-12 10:51:52 +0400

    Add files via upload

---
+++ proba2/proba/Form3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace proba
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 form = new Form1();
+            form.ShowDialog();
+        }
+    }
+}
